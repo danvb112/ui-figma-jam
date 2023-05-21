b5ae83592f67ea97cfbe108b348fcc0731b7ad8a
Daniel Bonasser <danielbonasser@gmail.com>
2023-05-20 21:15:12 -0300

    Add tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import ReactFlow, { Background, Controls } from 'reactflow';
+import {zinc} from 'tailwindcss/colors';
 import 'reactflow/dist/style.css';
 
 function App() {
 
   return (
-    <div style={{width: '100vw', height: '100vh'}}>
+    <div className='w-screen h-screen'>
       <ReactFlow>
         <Background 
           gap={12}
           size={2}
-          color='#ddd'
+          color={zinc[200]}
         />
       </ReactFlow>
     </div>
