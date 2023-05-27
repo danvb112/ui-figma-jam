5cec0098aab56e470d339e45c8b539a53ce65bfb
Daniel Bonasser <danielbonasser@gmail.com>
2023-05-27 10:49:00 -0300

    Add toolbar with button to add a new Node

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import ReactFlow, { Background, Connection, ConnectionMode, Controls, Node, addEdge, useEdgesState, useNodesState } from 'reactflow';
+import * as Toolbar from '@radix-ui/react-toolbar';
 import {zinc} from 'tailwindcss/colors';
 import 'reactflow/dist/style.css';
 import { Square } from './components/Square/Square';
@@ -42,6 +43,21 @@ function App() {
       return setEdges(edges => addEdge(connection, edges))
   }, []);
 
+  function addSquareNode() {
+    setNodes(nodes => [
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type: 'square',
+        position: {
+         x: 700,
+         y: 350,
+        },
+        data: {}
+       },
+    ])
+  }
+
   return (
     <div className='w-screen h-screen'>
       <ReactFlow
@@ -64,6 +80,13 @@ function App() {
         />
         <Controls />
       </ReactFlow>
+
+      <Toolbar.Root className='fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden'>
+        <Toolbar.Button 
+          onClick={addSquareNode}
+          className='w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-2'
+        />
+      </Toolbar.Root>
     </div>
   )
 }
